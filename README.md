c0c88898061ada77315d03ccb0d5acfb62d2a494
sabertazimi <sabertazimi@gmail.com>
2018-05-18 17:10:08 +0800

    fix(virt-mocker-format): transform spaces to tabs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mocker
 
 Minimal docker-like container
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/sabertazimi/mocker)](https://goreportcard.com/report/github.com/sabertazimi/mocker)
--- mocker.go
+++ mocker.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-    "log"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "strconv"
-    "syscall"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"syscall"
 )
 
 func main() {
-    switch os.Args[1] {
-    case "run":
-        run()
-    case "child":
-        child()
-    default:
-        log.Fatal("Invalid command")
-    }
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		log.Fatal("Invalid command")
+	}
 }
 
 func run() {
-    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.SysProcAttr = &syscall.SysProcAttr {
-        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-    }
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
 
-    must(cmd.Run())
+	must(cmd.Run())
 }
 
 func child() {
-    log.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+	log.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
 
-    cg()
+	cg()
 
-    cmd := exec.Command(os.Args[2], os.Args[3:]...)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-    must(syscall.Sethostname([]byte("mocker")))
-    must(syscall.Chroot("/home/sabertazimi/rootfs"))
-    must(syscall.Chdir("/"))
-    must(syscall.Mount("proc", "proc", "proc", 0, ""))
-    must(cmd.Run())
-    must(syscall.Unmount("proc", 0))
+	must(syscall.Sethostname([]byte("mocker")))
+	must(syscall.Chroot("/home/sabertazimi/rootfs"))
+	must(syscall.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	must(cmd.Run())
+	must(syscall.Unmount("proc", 0))
 }
 
 func cg() {
-    cgroups := "/sys/fs/cgroup/"
-    pids := filepath.Join(cgroups, "pids")
-    os.Mkdir(filepath.Join(pids, "sabertazimi"), 0755)
-    must(ioutil.WriteFile(filepath.Join(pids, "sabertazimi/pids.max"), []byte("20"), 0700))
-    must(ioutil.WriteFile(filepath.Join(pids, "sabertazimi/notify_on_release"), []byte("1"), 0700))
-    must(ioutil.WriteFile(filepath.Join(pids, "sabertazimi/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "sabertazimi"), 0755)
+	must(ioutil.WriteFile(filepath.Join(pids, "sabertazimi/pids.max"), []byte("20"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "sabertazimi/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "sabertazimi/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
